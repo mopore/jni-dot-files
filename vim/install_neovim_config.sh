920891808d1570aecea47db2751d5f311a781d7e
mopore <jni@mopore.org>
2022-06-12 16:42:22 +0200

    Bugfix for strange error.

--- vim/install_neovim_config.sh
+++ vim/install_neovim_config.sh
@@ -42,8 +42,6 @@ mv -v ./vimrc ~/.config/nvim/init.vim
 mkdir -p ~/.vim/undodir
 
 # Automatically lunch Plugin installation for referenced plugins in config.
-# vim -c ':PlugInstall | quit | quit'
-# clear
-# echo "Vim is ready!"
+vim -c ':PlugInstall | quit | quit'
 
 exit 0
