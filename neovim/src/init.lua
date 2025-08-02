63888552056563f210a20d0ded36b70e589be3f8
mopore <jni@mopore.org>
2025-08-02 15:49:07 +0200

    Add softtabstop

--- neovim/src/init.lua
+++ neovim/src/init.lua
@@ -385,6 +385,7 @@ vim.cmd.colorscheme "catppuccin"
 -- Set default tabstop to 4 -- JNI addition
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
 vim.o.expandtab = true
 
 -- Set highlight on search
