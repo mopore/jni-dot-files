ef96fb078b362453f953b0bdd5de23054f540752
mopore <jni@mopore.org>
2025-08-24 16:47:35 +0200

    Add plugin for a floating command line

--- neovim/src/init.lua
+++ neovim/src/init.lua
@@ -402,8 +402,31 @@ require('lazy').setup({
       {"<leader><space>", function () require("snipe").open_buffer_menu() end, desc = "Open Snipe buffer menu"}
     },
     opts = {}
-  }
+  },
 
+  {
+    --   ____               _ _     _            
+    --  / ___|_ __ ___   __| | |   (_)_ __   ___ 
+    -- | |   | '_ ` _ \ / _` | |   | | '_ \ / _ \
+    -- | |___| | | | | | (_| | |___| | | | |  __/
+    --  \____|_| |_| |_|\__,_|_____|_|_| |_|\___|
+    --                                           
+    "VonHeikemen/fine-cmdline.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    keys = {
+      { ":", "<cmd>FineCmdline<CR>", mode = "n", desc = "Floating cmdline" },
+    },
+    opts = {
+      cmdline = { enable_keymaps = true, smart_history = true, prompt = ": " },
+      popup = {
+        position = { row = "45%", col = "50%" },
+        size = { width = "60%" },
+        border = { style = "rounded" },
+        win_options = { winhighlight = "Normal:Normal,FloatBorder:FloatBorder" },
+      },
+    },
+    config = function(_, opts) require("fine-cmdline").setup(opts) end,
+  },
 
 }, {})
 
