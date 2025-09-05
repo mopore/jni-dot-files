e342c7cdb8f498295af3b7a1cd159c9f2d8f951a
mopore <jni@mopore.org>
2025-09-05 07:14:05 +0200

    Raise the font size for old people

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,7 @@ end)
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 16.0
+config.font_size = 18.0
 config.window_close_confirmation = 'NeverPrompt'
 
 config.window_background_opacity = 0.9
