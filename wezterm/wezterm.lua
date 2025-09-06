ab9afc0fe4cefdf4fdfd979549045f8a55b364d8
mopore <jni@mopore.org>
2025-09-06 09:46:52 +0200

    Add example for centering and resizing window on startup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,6 +5,19 @@ wezterm.on("format-window-title", function()
   return "λ - hack!"
 end)
 
+-- Place and size the window on startup
+wezterm.on("gui-startup", function(cmd)
+  local _, _, window = wezterm.mux.spawn_window(cmd or {})
+  local screen_w = 3440
+  local screen_h = 1440
+  local win_w = 1219
+  local win_h = 1295
+  local pos_x = math.floor((screen_w - win_w) / 2)
+  local pos_y = math.floor((screen_h - win_h) / 2) + 45
+
+  window:gui_window():set_position(pos_x, pos_y)
+  window:gui_window():set_inner_size(win_w, win_h)
+end)
 local c = wezterm.config_builder()
 
 -- c.enable_wayland = false -- If you want window decorations on Wayland (switch to X11)
