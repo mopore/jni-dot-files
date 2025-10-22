b8b9fd7a46e417dcc4163dfe7ccedc131af9ba39
mopore <jni@mopore.org>
2025-10-22 18:51:56 +0200

    Fix issue with missing keybinds for typescript.

--- nvim/src/lua/config/plugins/lspconfig.lua
+++ nvim/src/lua/config/plugins/lspconfig.lua
@@ -18,32 +18,6 @@ return {
     },
 
     config = function()
-      -- Your on_attach is unchanged
-      local on_attach = function(_, bufnr)
-        print("LSP connected.")
-        local nmap = function(keys, func, desc)
-          if desc then desc = "LSP: " .. desc end
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-        end
-
-        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        nmap("<leader>ss", require("telescope.builtin").lsp_document_symbols, "[S]earch [S]ymbols")
-        nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-        nmap("<M-k>", vim.lsp.buf.hover, "Hover Documentation")
-        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-        nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-        nmap("<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "[W]orkspace [L]ist Folders")
-
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_) vim.lsp.buf.format() end,
-          { desc = "Format current buffer with LSP" })
-      end
 
       -- neodev first
       require("neodev").setup()
@@ -81,7 +55,6 @@ return {
       if vim.fn.has("nvim-0.11") == 1 and vim.lsp and vim.lsp.config then
         vim.lsp.config("*", {
           capabilities = capabilities,
-          on_attach = on_attach,
         })
       end
 
@@ -102,8 +75,33 @@ return {
         automatic_enable = true, -- default; shown here for clarity
       })
 
-      -- If you prefer to control enablement yourself, disable automatic_enable and do:
-      -- for name, _ in pairs(servers) do vim.lsp.enable(name) end
+      -- This will always be exectued the moment an lsp attaches.
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("jni-lsp-maps", { clear = true }),
+        callback = function(args)
+          local bufnr = args.buf
+          local nmap = function(keys, func, desc)
+            if desc then desc = "LSP: " .. desc end
+            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc, silent = true })
+          end
+
+          nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+          nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+          nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+          nmap("<leader>ss", require("telescope.builtin").lsp_document_symbols, "[S]earch [S]ymbols")
+          nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+          nmap("<M-k>", vim.lsp.buf.hover, "Hover Documentation")
+          nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+          nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+          nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+          vim.api.nvim_buf_create_user_command(bufnr, "Format", function() vim.lsp.buf.format() end,
+            { desc = "Format current buffer with LSP" })
+        end,
+      })
     end,
   },
 }
